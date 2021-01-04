8756a4b1995fdc88b4895a5b538b395de86c29b2
Sami Hattab <hattab_sami@yahoo.fr>
2021-01-04 12:31:31 +0100

    add simple error management and redirect

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+$("#submit").on("click", function (e) {
+    e.preventDefault();
+
+    const user = $("#user").val();
+    const password = $("#password").val();
+
+    $.ajax({
+        url: `http://greenvelvet.alwaysdata.net/kwick/api/signup/${user}/${password}`,
+        dataType: "json"
+    })
+        .then((res) => {
+            console.log(res)
+
+            if(res.result.status === "failure") {
+                alert(res.result.message);
+            } else {
+                window.location.replace("pages/sign-in.html")
+            }
+        })
+})
